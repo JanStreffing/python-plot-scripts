4aa03e02c475661c3044b8ab00ea1fd47806dffd
Jan Streffing <jan.streffing@awi.de>
2019-11-26 14:55:25 +0100

    reorganized plots to have resolution next to each other

--- PAMIP_res_paper/nh_stereo_diff_welch.py
+++ PAMIP_res_paper/nh_stereo_diff_welch.py
@@ -40,139 +40,140 @@ if str(sys.argv[9]) == "true":
 
 
 if __name__ == '__main__':
-    exp1=str(sys.argv[1])
-    exp2=str(sys.argv[2])
-    res=str(sys.argv[3])
-    basepath=str(sys.argv[6])
-    outpath=str(sys.argv[8])
-    datapath1=basepath+res+'/Experiment_'+exp1+'/ensemble_mean/'
-    datapath2=basepath+res+'/Experiment_'+exp2+'/ensemble_mean/'
-    datapath3=basepath+res+'/Experiment_'+exp1+'/'
-    datapath4=basepath+res+'/Experiment_'+exp2+'/'    
-    
-
-    if str(sys.argv[11]) == "colorbar_TR_15":
-       from colorbar_TR_15 import cmap_TR
-    if str(sys.argv[11]) == "colorbar_TR_70":
-       from colorbar_TR_70 import cmap_TR
-
-    param=str(sys.argv[4])
-    paramname=str(sys.argv[5])
-    mapticks=map(float, sys.argv[10].split(','))
-
-    itimes=0
-    fig = plt.figure(figsize=(8,8))
-    
-    for season in ['_DJF','_MAM', '_JJA', '_SON']:
-
-       dataset3=[]
-       dataset4=[]
-       data3=[]
-       data4=[]
-
-       # Reading netcdf files
-       ncfile1 = datapath1+paramname+'_ensmean'+season+'.nc'
-       ncfile2 = datapath2+paramname+'_ensmean'+season+'.nc'
-       dataset1 = Dataset(ncfile1) 
-       dataset2 = Dataset(ncfile2) 
-       print ncfile1
-       print ncfile2
-
-       if str(sys.argv[9]) == "true":
-          for i in range(100):
-             ncfile3 = datapath3+'E'+str(i+1).zfill(3)+'/outdata/oifs/seasonal_mean/'+paramname+season+'.nc'
-             ncfile4 = datapath4+'E'+str(i+1).zfill(3)+'/outdata/oifs/seasonal_mean/'+paramname+season+'.nc'
-             print ncfile3
-             dataset3.append(Dataset(ncfile3))
-             dataset4.append(Dataset(ncfile4))
-
-       
-       # Loading data from datasets
-       data1 = dataset1.variables[param][:]
-       data2 = dataset2.variables[param][:]
-       if str(sys.argv[9]) == "true":
-          for i in range(100):
-             data3.append(dataset3[i].variables[param][:])
-             data4.append(dataset4[i].variables[param][:])
+	exp1=str(sys.argv[1])
+	exp2=str(sys.argv[2])
+	season=str(sys.argv[3])
+	basepath=str(sys.argv[6])
+	outpath=str(sys.argv[8])
 
 
-       # Calculating Welch T-test
-       welch = stats.ttest_ind(data3,data4)
-       print(np.isnan(data3).any())
-       print(np.isnan(data4).any())
+	if str(sys.argv[11]) == "colorbar_TR_15":
+		from colorbar_TR_15 import cmap_TR
+	if str(sys.argv[11]) == "colorbar_TR_70":
+		from colorbar_TR_70 import cmap_TR
 
-       # in case data has multiple levels, select only the 6th one (50000 hPa)
-       print(np.squeeze(data1).shape)
-       print(len(np.squeeze(data1).shape))
-       if (len(np.squeeze(data1).shape)) == 3:
-          data1 = data1[0,5,:,:]
-          data2 = data2[0,5,:,:]
-          for i in range(100):
-             data3[i] =  data3[i][0,5,:,:]
+	param=str(sys.argv[4])
+	paramname=str(sys.argv[5])
+	mapticks=map(float, sys.argv[10].split(','))
+	reslist=map(str, sys.argv[13].split(','))
 
-       # Split data and concatenate in reverse order to turn by 180° to Prime meridian
-       ds1,ds2 = np.hsplit(np.squeeze(data1),2)
-       data_cat1 = np.concatenate((ds2,ds1),axis=1)/float(sys.argv[7])
-       ds1,ds2 = np.hsplit(np.squeeze(data2),2)
-       data_cat2 = np.concatenate((ds2,ds1),axis=1)/float(sys.argv[7])
-       if str(sys.argv[9]) == "true":
-          ds1,ds2 = np.hsplit(np.squeeze(welch[1]),2)
-          data_cat3 = np.concatenate((ds2,ds1),axis=1)
+	itimes=0
+	fig = plt.figure(figsize=(8,8))
 
-       # Loading coords, turning longitude coordiante by 180° to Prime meridian
-       lons = dataset1.variables[u'lon'][:]-180
-       lats = dataset1.variables[u'lat'][:]
+	for res in reslist:
 
-       # add cyclic point to data array and longitude
-       data_cat1 ,lons = Ngl.add_cyclic(data_cat1, lons)		
-       data_cat2 = Ngl.add_cyclic(data_cat2)	
-       if str(sys.argv[9]) == "true":
-          data_cat3 = Ngl.add_cyclic(data_cat3)	
-    
+		datapath1=basepath+res+'/Experiment_'+exp1+'/ensemble_mean/'
+		datapath2=basepath+res+'/Experiment_'+exp2+'/ensemble_mean/'
+		datapath3=basepath+res+'/Experiment_'+exp1+'/'
+		datapath4=basepath+res+'/Experiment_'+exp2+'/'    
+		dataset3=[]
+		dataset4=[]
+		data3=[]
+		data4=[]
 
-       # Calculate where the standard deviation of dataset 1 is larger than the difference between 1 and 2
-       if str(sys.argv[9]) == "true":
-          data4 = data_cat3 < 0.05
+		# Reading netcdf files
+		ncfile1 = datapath1+paramname+'_ensmean_'+season+'.nc'
+		ncfile2 = datapath2+paramname+'_ensmean_'+season+'.nc'
+		dataset1 = Dataset(ncfile1) 
+		dataset2 = Dataset(ncfile2) 
+		print ncfile1
+		print ncfile2
 
-
-       # Set position of subplot and some general settings for cartopy
-       ax=plt.subplot(2,2,itimes+1,projection=ccrs.NorthPolarStereo())
-       ax.set_extent([-180, 180, 29, 29], ccrs.PlateCarree())
-       ax.add_feature(cfeature.COASTLINE)
-
-       # Configuring cartopy gridlines
-       gl = ax.gridlines(crs=ccrs.PlateCarree(), linewidth=0.3, color='black',  alpha=0.5)
-       gl.xlocator = mticker.FixedLocator([-180,-135,-90,-45,0,45,90,135,180])
-       gl.ylocator = mticker.FixedLocator([80, 60, 30, 0])
+		if str(sys.argv[9]) == "true":
+		  for i in range(100):
+		     ncfile3 = datapath3+'E'+str(i+1).zfill(3)+'/outdata/oifs/seasonal_mean/'+paramname+'_'+season+'.nc'
+		     ncfile4 = datapath4+'E'+str(i+1).zfill(3)+'/outdata/oifs/seasonal_mean/'+paramname+'_'+season+'.nc'
+		     print ncfile3
+		     dataset3.append(Dataset(ncfile3))
+		     dataset4.append(Dataset(ncfile4))
 
 
+		# Loading data from datasets
+		data1 = dataset1.variables[param][:]
+		data2 = dataset2.variables[param][:]
+		if str(sys.argv[9]) == "true":
+		  for i in range(100):
+		     data3.append(dataset3[i].variables[param][:])
+		     data4.append(dataset4[i].variables[param][:])
 
-       plt.tight_layout(pad=2)
-    
-       cmap_TR.set_over("darkred")
-       cmap_TR.set_under("deeppink")
 
-       if str(sys.argv[9]) == "true":
-          im=plt.contourf(lons, lats, data4, hatches=[' ','...'],cmap=cmap_TR, extend='both',transform=ccrs.PlateCarree(),zorder=2, alpha=0)
-       im=plt.contourf(lons, lats, data_cat2-data_cat1, levels=mapticks, cmap=cmap_TR, extend='both',transform=ccrs.PlateCarree(),zorder=1)
-  
-       if ( itimes == 0 ):
-          plt.text(0.00, 1.03, 'DJF', horizontalalignment='left', fontsize=18, transform=ax.transAxes)
-       elif ( itimes == 1):
-          plt.text(0.00, 1.03, 'MAM', horizontalalignment='left', fontsize=18, transform=ax.transAxes)
-       elif ( itimes == 2):
-          plt.text(0.00, 1.03, 'JJA', horizontalalignment='left', fontsize=18, transform=ax.transAxes)
-       elif ( itimes == 3):
-          plt.text(0.00, 1.03, 'SON', horizontalalignment='left', fontsize=18, transform=ax.transAxes)
-       
-        
-       itimes=itimes+1
+		# Calculating Welch T-test
+		welch = stats.ttest_ind(data3,data4)
+		print(np.isnan(data3).any())
+		print(np.isnan(data4).any())
+
+		# in case data has multiple levels, select only the 6th one (50000 hPa)
+		print(np.squeeze(data1).shape)
+		print(len(np.squeeze(data1).shape))
+		if (len(np.squeeze(data1).shape)) == 3:
+		  data1 = data1[0,5,:,:]
+		  data2 = data2[0,5,:,:]
+		  for i in range(100):
+		     data3[i] =  data3[i][0,5,:,:]
+
+		# Split data and concatenate in reverse order to turn by 180° to Prime meridian
+		ds1,ds2 = np.hsplit(np.squeeze(data1),2)
+		data_cat1 = np.concatenate((ds2,ds1),axis=1)/float(sys.argv[7])
+		ds1,ds2 = np.hsplit(np.squeeze(data2),2)
+		data_cat2 = np.concatenate((ds2,ds1),axis=1)/float(sys.argv[7])
+		if str(sys.argv[9]) == "true":
+		  ds1,ds2 = np.hsplit(np.squeeze(welch[1]),2)
+		  data_cat3 = np.concatenate((ds2,ds1),axis=1)
+
+		# Loading coords, turning longitude coordiante by 180° to Prime meridian
+		lons = dataset1.variables[u'lon'][:]-180
+		lats = dataset1.variables[u'lat'][:]
+
+		# add cyclic point to data array and longitude
+		data_cat1 ,lons = Ngl.add_cyclic(data_cat1, lons)		
+		data_cat2 = Ngl.add_cyclic(data_cat2)	
+		if str(sys.argv[9]) == "true":
+		  data_cat3 = Ngl.add_cyclic(data_cat3)	
+
+
+		# Calculate where the standard deviation of dataset 1 is larger than the difference between 1 and 2
+		if str(sys.argv[9]) == "true":
+		  data4 = data_cat3 < 0.05
+
+
+		# Set position of subplot and some general settings for cartopy
+		ax=plt.subplot(1,len(reslist),itimes+1,projection=ccrs.NorthPolarStereo())
+		ax.set_extent([-180, 180, 29, 29], ccrs.PlateCarree())
+		ax.add_feature(cfeature.COASTLINE)
+
+		# Configuring cartopy gridlines
+		gl = ax.gridlines(crs=ccrs.PlateCarree(), linewidth=0.3, color='black',  alpha=0.5)
+		gl.xlocator = mticker.FixedLocator([-180,-135,-90,-45,0,45,90,135,180])
+		gl.ylocator = mticker.FixedLocator([80, 60, 30, 0])
+
+
+
+		plt.tight_layout(pad=2)
+
+		cmap_TR.set_over("darkred")
+		cmap_TR.set_under("deeppink")
+
+		if str(sys.argv[9]) == "true":
+		  im=plt.contourf(lons, lats, data4, hatches=[' ','...'],cmap=cmap_TR, extend='both',transform=ccrs.PlateCarree(),zorder=2, alpha=0)
+		im=plt.contourf(lons, lats, data_cat2-data_cat1, levels=mapticks, cmap=cmap_TR, extend='both',transform=ccrs.PlateCarree(),zorder=1)
+
+		if ( itimes == 0 ):
+		  plt.text(0.00, 1.03, 'TL159', horizontalalignment='left', fontsize=18, transform=ax.transAxes)
+		elif ( itimes == 1):
+		  plt.text(0.00, 1.03, 'TL511', horizontalalignment='left', fontsize=18, transform=ax.transAxes)
+		elif ( itimes == 2):
+		  plt.text(0.00, 1.03, 'TL1279', horizontalalignment='left', fontsize=18, transform=ax.transAxes)
+		elif ( itimes == 3):
+		  plt.text(0.00, 1.03, 'SON', horizontalalignment='left', fontsize=18, transform=ax.transAxes)
+
+
+		itimes=itimes+1
     
 fig.subplots_adjust(left=0.01, right=0.85, bottom=0, top=0.98, wspace = 0.1, hspace=-0.1)
 cbar_ax = fig.add_axes([0.88, 0.06, 0.03, 0.87])
 cbar_ax.tick_params(labelsize=int(sys.argv[12])) 
 fig.colorbar(im, cax=cbar_ax, orientation='vertical', extend='both',ticks=mapticks)
-fig.savefig(outpath+paramname+'_'+exp2+'_'+exp1+'_'+res+'_map_diff.png')
+fig.savefig(outpath+paramname+'_'+exp2+'_'+exp1+'_'+season+'_map_diff.png')
 
 
 
--- PAMIP_res_paper/welch_pamip_postprocessing.sh
+++ PAMIP_res_paper/welch_pamip_postprocessing.sh
@@ -21,61 +21,60 @@ out_juwels=""
 #   Fontsize of colorbar ticks
 
 
-for todo in NAO #NAO SD T2M z500 MSL MSL U V synact
+for todo in T2M #NAO SD T2M z500 MSL MSL U V synact
 do
-	# z500 hPa polar plots 
-	if [[ "$todo" == "z500" ]]; then
-		python nh_stereo_diff_welch.py 11 16 T159 Z z500 $in_mistral 9.81 $out_mistral true -30,-26,-22,-18,-14,-10,-6,-2,2,6,10,14,18,22,26,30 colorbar_TR_15 18
-		python nh_stereo_diff_welch.py 11 16 T511 Z z500 $in_mistral 9.81 $out_mistral true -30,-26,-22,-18,-14,-10,-6,-2,2,6,10,14,18,22,26,30 colorbar_TR_15 18
-	fi
+	for season in DJF MAM JJA SON
+	do
+		# z500 hPa polar plots 
+		if [[ "$todo" == "z500" ]]; then
+			python nh_stereo_diff_welch.py 11 16 T159 Z z500 $in_mistral 9.81 $out_mistral true -30,-26,-22,-18,-14,-10,-6,-2,2,6,10,14,18,22,26,30 colorbar_TR_15 18
+			python nh_stereo_diff_welch.py 11 16 T511 Z z500 $in_mistral 9.81 $out_mistral true -30,-26,-22,-18,-14,-10,-6,-2,2,6,10,14,18,22,26,30 colorbar_TR_15 18
+		fi
 
 
-	# t2m hPa polar plots 
-	if [[ "$todo" == "T2M" ]]; then
-		python nh_stereo_diff_welch.py 11 16 T159 T2M T2M $in_mistral 1 $out_mistral true -7,-5,-3,-1,-0.5,-0.3,-0.1,0.1,0.3,0.5,1,3,5,7 colorbar_TR_70 18
-                python nh_stereo_diff_welch.py 11 16 T511 T2M T2M $in_mistral 1 $out_mistral true -7,-5,-3,-1,-0.5,-0.3,-0.1,0.1,0.3,0.5,1,3,5,7 colorbar_TR_70 18
-	fi
+		# t2m hPa polar plots 
+		if [[ "$todo" == "T2M" ]]; then
+			python nh_stereo_diff_welch.py 11 16 $season T2M T2M $in_mistral 1 $out_mistral true -7,-5,-3,-1,-0.5,-0.3,-0.1,0.1,0.3,0.5,1,3,5,7 colorbar_TR_70 18 T159,T511
+		fi
 
 
-	# MSL polar plots
-	if [[ "$todo" == "MSL" ]]; then
-		python nh_stereo_diff_welch.py 11 16 T159 MSL MSL $in_mistral 1 $out_mistral true -300,-260,-220,-180,-140,-100,-60,-20,20,60,100,140,180,220,260,300 colorbar_TR_15 16
-		python nh_stereo_diff_welch.py 11 16 T511 MSL MSL $in_mistral 1 $out_mistral true -300,-260,-220,-180,-140,-100,-60,-20,20,60,100,140,180,220,260,300 colorbar_TR_15 16
-	fi
+		# MSL polar plots
+		if [[ "$todo" == "MSL" ]]; then
+			python nh_stereo_diff_welch.py 11 16 T159 MSL MSL $in_mistral 1 $out_mistral true -300,-260,-220,-180,-140,-100,-60,-20,20,60,100,140,180,220,260,300 colorbar_TR_15 16
+			python nh_stereo_diff_welch.py 11 16 T511 MSL MSL $in_mistral 1 $out_mistral true -300,-260,-220,-180,-140,-100,-60,-20,20,60,100,140,180,220,260,300 colorbar_TR_15 16
+		fi
 
 
-	# U polar plots
-	if [[ "$todo" == "U" ]]; then
-		python nh_stereo_diff_welch.py 11 16 T159 U U $in_mistral 1 $out_mistral true -1.5,-1.3,-1.1,-0.9,-0.7,-0.5,-0.3,-0.1,0.1,0.3,0.5,0.7,0.9,1.1,1.3,1.5 colorbar_TR_15 18
-		python nh_stereo_diff_welch.py 11 16 T511 U U $in_mistral 1 $out_mistral true -1.5,-1.3,-1.1,-0.9,-0.7,-0.5,-0.3,-0.1,0.1,0.3,0.5,0.7,0.9,1.1,1.3,1.5 colorbar_TR_15 18
-	fi
+		# U polar plots
+		if [[ "$todo" == "U" ]]; then
+			python nh_stereo_diff_welch.py 11 16 T159 U U $in_mistral 1 $out_mistral true -1.5,-1.3,-1.1,-0.9,-0.7,-0.5,-0.3,-0.1,0.1,0.3,0.5,0.7,0.9,1.1,1.3,1.5 colorbar_TR_15 18
+			python nh_stereo_diff_welch.py 11 16 T511 U U $in_mistral 1 $out_mistral true -1.5,-1.3,-1.1,-0.9,-0.7,-0.5,-0.3,-0.1,0.1,0.3,0.5,0.7,0.9,1.1,1.3,1.5 colorbar_TR_15 18
+		fi
 
-        # V polar plots
-        if [[ "$todo" == "V" ]]; then
-                python nh_stereo_diff_welch.py 11 16 T159 V V $in_mistral 1 $out_mistral true -1.5,-1.3,-1.1,-0.9,-0.7,-0.5,-.3,-.1,.1,.3,0.5,0.7,0.9,1.1,1.3,1.5 colorbar_TR_15 18
-                python nh_stereo_diff_welch.py 11 16 T511 V V $in_mistral 1 $out_mistral true -1.5,-1.3,-1.1,-0.9,-0.7,-0.5,-.3,-.1,.1,.3,0.5,0.7,0.9,1.1,1.3,1.5 colorbar_TR_15 18
-        fi
+		# V polar plots
+		if [[ "$todo" == "V" ]]; then
+			python nh_stereo_diff_welch.py 11 16 T159 V V $in_mistral 1 $out_mistral true -1.5,-1.3,-1.1,-0.9,-0.7,-0.5,-.3,-.1,.1,.3,0.5,0.7,0.9,1.1,1.3,1.5 colorbar_TR_15 18
+			python nh_stereo_diff_welch.py 11 16 T511 V V $in_mistral 1 $out_mistral true -1.5,-1.3,-1.1,-0.9,-0.7,-0.5,-.3,-.1,.1,.3,0.5,0.7,0.9,1.1,1.3,1.5 colorbar_TR_15 18
+		fi
 
-        # synoptic activity polar plots
-        if [[ "$todo" == "synact" ]]; then
-                python nh_stereo_diff_welch.py 11 16 T159 Z synact $in_mistral 9.81 $out_mistral true -7,-5,-3,-1,-0.5,-0.3,-0.1,0.1,0.3,0.5,1,3,5,7 colorbar_TR_70 18
-                python nh_stereo_diff_welch.py 11 16 T511 Z synact $in_mistral 9.81 $out_mistral true -7,-5,-3,-1,-0.5,-0.3,-0.1,0.1,0.3,0.5,1,3,5,7 colorbar_TR_70 18
-        fi
+		# synoptic activity polar plots
+		if [[ "$todo" == "synact" ]]; then
+			python nh_stereo_diff_welch.py 11 16 T159 Z synact $in_mistral 9.81 $out_mistral true -7,-5,-3,-1,-0.5,-0.3,-0.1,0.1,0.3,0.5,1,3,5,7 colorbar_TR_70 18
+			python nh_stereo_diff_welch.py 11 16 T511 Z synact $in_mistral 9.81 $out_mistral true -7,-5,-3,-1,-0.5,-0.3,-0.1,0.1,0.3,0.5,1,3,5,7 colorbar_TR_70 18
+		fi
 
-	if [[ "$todo" == "SD" ]]; then
-                python nh_stereo_diff_welch.py 11 16 T159 SD SD $in_mistral 0.01 $out_mistral true -3,-2,-1.5,-1,-0.5,-0.2,0.2,0.5,1,1.5,2,3 colorbar_TR_15 18
-                python nh_stereo_diff_welch.py 11 16 T511 SD SD $in_mistral 0.01 $out_mistral true -3,-2,-1.5,-1,-0.5,-0.2,0.2,0.5,1,1.5,2,3 colorbar_TR_15 18
-	fi
+		if [[ "$todo" == "SD" ]]; then
+			python nh_stereo_diff_welch.py 11 16 T159 SD SD $in_mistral 0.01 $out_mistral true -3,-2,-1.5,-1,-0.5,-0.2,0.2,0.5,1,1.5,2,3 colorbar_TR_15 18
+			python nh_stereo_diff_welch.py 11 16 T511 SD SD $in_mistral 0.01 $out_mistral true -3,-2,-1.5,-1,-0.5,-0.2,0.2,0.5,1,1.5,2,3 colorbar_TR_15 18
+		fi
 
-        if [[ "$todo" == "NAO" ]]; then
-		for season in DJF MAM JJA SON; do
+		if [[ "$todo" == "NAO" ]]; then
 			for res in T511 T159; do
 				python nao_ngl_diff.py 11 16 $res MSL MSL $in_mistral 1 $out_mistral false -0.8,-0.7,-0.6,-0.5,-0.4,-0.3,-0.2,-0.1,-0.05,-0.03,-0.01,0.01,0.03,0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8 $season
 				python nao_ngl.py 11 1 $res MSL MSL $in_mistral 1 $out_mistral false -3,-2.5,-2,-1.5,-1,-.7,-.4,-.2,-.1,.1,.2,.4,.7,1,1.5,2,2.5,3 $season
 				python nao_ngl.py 16 1 $res MSL MSL $in_mistral 1 $out_mistral false -3,-2.5,-2,-1.5,-1,-.7,-.4,-.2,-.1,.1,.2,.4,.7,1,1.5,2,2.5,3 $season
 			done
-		done
-        fi
-
+		fi
+	done
 
 done
