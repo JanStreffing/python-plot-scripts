ed73957f5670271e0fa872e0a69b1c674793de3e
JanStreffing <jan.streffing@awi.de>
2020-02-28 01:59:17 +0100

    starting similarity measure

---
+++ PAMIP_res_paper/similarity.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+in="/p/largedata/hhb19/jstreffi/runtime/oifsamip/"
+out="/p/project/chhb19/jstreffi/postprocessing/PAMIP/"
+
+
+# Argument list:
+#   Experiment id 1
+#   Experiment id 2
+#   Resolution
+#   Variable name in netcdf files
+#   Variable name in name of netcdf file
+#   Input path of ensemble mean data
+#   Factor by which data should be divided
+#   Output path for plots / input path for ensstd data
+#   Bool for using ensstd or not
+#   List of tick on colorbar
+#   Name of custom colorbar
+#   Fontsize of colorbar ticks
+
+
+for todo in sim
+do
+
+        if [[ "$todo" == "sim" ]]; then
+                python similarity_measure.py 11 16 T159 Z Z $in 1 $out 
+	fi
+done
---
+++ PAMIP_res_paper/similarity_measure.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 26 13:12:45 2040
+
+@author: jstreffi-local
+
+Input arguments:
+	1 	Id of first experiment
+	2	Id of second experiment
+	3	Resulution of experiments
+	4 	Parameter name in netcdf file
+	5 	Parameter name in name of netcdf file
+	6 	Basepath of experiments on this machine
+	7	Number by which the variable should be diveded before plotting (e.g 9.81 for pressure)
+	8 	Plotoutput path on this machine
+	9	Name of colormap module
+	10	List of colorbar points
+"""
+from __future__ import division
+import sys
+import random as rd
+import numpy as np
+import bootstrapped.bootstrap as bs
+import bootstrapped.stats_functions as bs_stats
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.pylab import *
+from netCDF4 import Dataset
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import Ngl
+from tqdm import tqdm
+import dask
+from dask.delayed import delayed
+from dask.diagnostics import ProgressBar
+from ttictoc import TicToc
+t = TicToc() ## TicToc("name")
+
+
+def resample_inner_loop(data):
+	resample_inner = []
+	#resample_inner = np.array([])
+	for n in range(1,len(data)+1):
+		rx = []
+		for k in range(n):
+			r = rd.randrange(0, len(data))
+			rx.append(data[r])
+		resample_inner.append(np.ma.mean(rx, axis=0))
+	return resample_inner
+
+
+def resample(data):
+	B=10
+	res = []
+	for b in range(B):
+		res.append(dask.delayed(resample_inner_loop)(data))
+        with ProgressBar():
+                resample = dask.compute(res)
+		resample_itermean = mean(resample,axis=1)
+	return np.squeeze(np.asarray(resample_itermean))
+
+
+def read_inner(ncfile1, param):
+	data1 = Dataset(ncfile1).variables[param][:]
+	print (np.asarray(data1).shape)
+	return data1
+
+
+def read_file_par(ensnumber,datapath1,paramname,param):
+	data1 = []
+	for i in range(ensnumber):
+		ncfile1 = datapath1+'E'+str(i+1).zfill(3)+'/outdata/oifs/monthly_mean/'+paramname+'_monmean.nc'			
+		data1.append(dask.delayed(read_inner)(ncfile1, param))
+        with ProgressBar():
+               	d = dask.compute(data1)
+	return d
+
+
+def read_file_squ(ensnumber,datapath1,paramname,param):
+	d = []
+	for i in tqdm(range(ensnumber)):
+		ncfile1 = datapath1+'E'+str(i+1).zfill(3)+'/outdata/oifs/monthly_mean/'+paramname+'_monmean.nc'			
+		d.append(read_inner(ncfile1, param))
+	return d
+
+
+
+if __name__ == '__main__':
+	exp1=str(sys.argv[1])
+	exp2=str(sys.argv[2])
+	basepath=str(sys.argv[6])
+	outpath=str(sys.argv[8])
+
+	param=str(sys.argv[4])
+	paramname=str(sys.argv[5])
+	reslist=map(str, sys.argv[3].split(','))
+	print(reslist)
+	itimes=0
+	fig =  plt.figure(figsize=(9,10.6875))
+
+	for res in reslist:
+		if res == 'T1279':
+			ensnumber = 100
+		else:
+			ensnumber = 300
+		datapath1=basepath+res+'/Experiment_'+exp1+'/'
+		#datapath2=basepath+res+'/Experiment_'+exp2+'/'    
+		data1=[]
+		#data2=[]
+
+		
+		#d = read_file_par(ensnumber,datapath1,paramname,param)
+		d = read_file_squ(ensnumber,datapath1,paramname,param)
+
+		print(np.asarray(d).shape)
+# --- Resampling	
+		res = resample(data1)
+		print(np.asarray(res).shape)
+
+		mean = np.mean(np.asarray(data1), axis=0)
+		rmse = []
+		for i in range(0,len(res)-1):
+			rmse.append(np.sqrt(((res[i] - mean) ** 2).mean()))
+		print(rmse)
