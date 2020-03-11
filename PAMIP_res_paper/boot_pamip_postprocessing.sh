dce0087dc004b4ded6cc728647af5b2378a66b4f
JanStreffing <jan.streffing@awi.de>
2020-03-11 16:19:43 +0100

    towards final form of bootstrap testing map plots script

--- PAMIP_res_paper/boot_pamip_postprocessing.sh
+++ PAMIP_res_paper/boot_pamip_postprocessing.sh
@@ -3,8 +3,8 @@
 module unload python
 #conda activate pyn_env_py2
 
-in_mistral="/mnt/lustre01/work/ab0246/a270092/runtime/oifsamip/"
-out_mistral="/mnt/lustre01/work/ab0246/a270092/postprocessing/PAMIP/"
+input_path="/p/largedata/hhb19/jstreffi/runtime/oifsamip/"
+output_path="/p/project/chhb19/jstreffi/postprocessing/PAMIP/"
 
 
 # Argument list:
@@ -22,67 +22,67 @@ out_mistral="/mnt/lustre01/work/ab0246/a270092/postprocessing/PAMIP/"
 #   Fontsize of colorbar ticks
 
 
-for todo in MSL T2M MSL z500 forcing #haus U SD forcing synact
+for todo in MSL Z T2M #haus U SD forcing synact
 do
 	# z500 hPa polar plots 
-	if [[ "$todo" == "z500" ]]; then
-		python nh_stereo_diff_bootstrap.py 11 16 T159,T511,T1279 Z z500 $in_mistral 9.81 $out_mistral true -30,-26,-22,-18,-14,-10,-6,-2,2,6,10,14,18,22,26,30 colorbar_TR_15 18 
+	if [[ "$todo" == "Z" ]]; then
+		python nh_stereo_diff_bootstrap.py 11 16 T159,T511,T1279 Z Z $input_path 9.81 $output_path true -30,-26,-22,-18,-14,-10,-6,-2,2,6,10,14,18,22,26,30 colorbar_TR_15 18 
 	fi
 
 
 	# t2m hPa polar plots 
 	if [[ "$todo" == "T2M" ]]; then
-		python nh_stereo_diff_bootstrap.py 11 16 T159,T511,T1279 T2M T2M $in_mistral 1 $out_mistral true -7,-5,-3,-1,-0.5,-0.3,-0.1,0.1,0.3,0.5,1,3,5,7 colorbar_TR_70 18 
+		python nh_stereo_diff_bootstrap.py 11 16 T159,T511,T1279 T2M T2M $input_path 1 $output_path true -7,-5,-3,-1,-0.5,-0.3,-0.1,0.1,0.3,0.5,1,3,5,7 colorbar_TR_70 18 
 	fi
 
 
 	# MSL polar plots
 	if [[ "$todo" == "MSL" ]]; then
-		python nh_stereo_diff_bootstrap.py 11 16 T159,T511,T1279 MSL MSL $in_mistral 1 $out_mistral true -300,-260,-220,-180,-140,-100,-60,-20,20,60,100,140,180,220,260,300 colorbar_TR_15 16 
+		python nh_stereo_diff_bootstrap.py 11 16 T159,T511,T1279 MSL MSL $input_path 1 $output_path true -300,-260,-220,-180,-140,-100,-60,-20,20,60,100,140,180,220,260,300 colorbar_TR_15 17 
 	fi
 
 
 	# U polar plots
 	if [[ "$todo" == "U" ]]; then
-		python nh_stereo_diff_bootstrap.py 11 16 T159,T511,T1279 U U $in_mistral 1 $out_mistral true -1.5,-1.3,-1.1,-0.9,-0.7,-0.5,-0.3,-0.1,0.1,0.3,0.5,0.7,0.9,1.1,1.3,1.5 colorbar_TR_15 18  
+		python nh_stereo_diff_bootstrap.py 11 16 T159,T511,T1279 U U $input_path 1 $output_path true -1.5,-1.3,-1.1,-0.9,-0.7,-0.5,-0.3,-0.1,0.1,0.3,0.5,0.7,0.9,1.1,1.3,1.5 colorbar_TR_15 18  
 	fi
 
 	# V polar plots
 	if [[ "$todo" == "V" ]]; then
-		python nh_stereo_diff_bootstrap.py 11 16 T159,T511,T1279 V V $in_mistral 1 $out_mistral true -1.5,-1.3,-1.1,-0.9,-0.7,-0.5,-.3,-.1,.1,.3,0.5,0.7,0.9,1.1,1.3,1.5 colorbar_TR_15 18 
+		python nh_stereo_diff_bootstrap.py 11 16 T159,T511,T1279 V V $input_path 1 $output_path true -1.5,-1.3,-1.1,-0.9,-0.7,-0.5,-.3,-.1,.1,.3,0.5,0.7,0.9,1.1,1.3,1.5 colorbar_TR_15 18 
 	fi
 
 	# synoptic activity polar plots
 	if [[ "$todo" == "synact" ]]; then
-		python nh_stereo_diff_bootstrap.py 11 16 T159,T511,T1279 Z synact $in_mistral 9.81 $out_mistral true -7,-5,-3,-1,-0.5,-0.3,-0.1,0.1,0.3,0.5,1,3,5,7 colorbar_TR_70 18 
+		python nh_stereo_diff_bootstrap.py 11 16 T159,T511,T1279 Z synact $input_path 9.81 $output_path true -7,-5,-3,-1,-0.5,-0.3,-0.1,0.1,0.3,0.5,1,3,5,7 colorbar_TR_70 18 
 	fi
 
 	if [[ "$todo" == "SD" ]]; then
-		python nh_stereo_diff_bootstrap.py 11 16 T159,T511,T1279 SD SD $in_mistral 0.01 $out_mistral true -1.5,-1.3,-1.1,-0.9,-0.7,-0.5,-.3,-.1,.1,.3,0.5,0.7,0.9,1.1,1.3,1.5 colorbar_TR_15 18 
+		python nh_stereo_diff_bootstrap.py 11 16 T159,T511,T1279 SD SD $input_path 0.01 $output_path true -1.5,-1.3,-1.1,-0.9,-0.7,-0.5,-.3,-.1,.1,.3,0.5,0.7,0.9,1.1,1.3,1.5 colorbar_TR_15 18 
 	fi
 
 	if [[ "$todo" == "PRECIP" ]]; then
-		python nh_stereo_diff_bootstrap.py 11 16 T159,T511,T1279 PRECIP PRECIP $in_mistral 0.00025 $out_mistral true -1.1,-0.9,-0.7,-0.5,-.3,-.1,.1,.3,0.5,0.7,0.9,1.1 colorbar_TR_15 18 
+		python nh_stereo_diff_bootstrap.py 11 16 T159,T511,T1279 PRECIP PRECIP $input_path 0.00025 $output_path true -1.1,-0.9,-0.7,-0.5,-.3,-.1,.1,.3,0.5,0.7,0.9,1.1 colorbar_TR_15 18 
 	fi
 
 	if [[ "$todo" == "NAO" ]]; then
 		for season in DJF MAM JJA SON
 		do
 			for res in T511 T159 T1279; do
-				python nao_ngl_diff.py 11 16 $res MSL MSL $in_mistral 1 $out_mistral false -1.5,-1.3,-1.1,-0.9,-0.7,-0.5,-0.3,-0.1,0.1,0.3,0.5,0.7,0.9,1.1,1.3,1.5 $season
+				python nao_ngl_diff.py 11 16 $res MSL MSL $input_path 1 $output_path false -1.5,-1.3,-1.1,-0.9,-0.7,-0.5,-0.3,-0.1,0.1,0.3,0.5,0.7,0.9,1.1,1.3,1.5 $season
 				#-0.8,-0.7,-0.6,-0.5,-0.4,-0.3,-0.2,-0.1,-0.05,-0.03,-0.01,0.01,0.03,0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8
-				python nao_ngl.py 11 1 $res MSL MSL $in_mistral 1 $out_mistral false -3,-2.5,-2,-1.5,-1,-.7,-.4,-.2,-.1,.1,.2,.4,.7,1,1.5,2,2.5,3 $season
-				python nao_ngl.py 16 1 $res MSL MSL $in_mistral 1 $out_mistral false -3,-2.5,-2,-1.5,-1,-.7,-.4,-.2,-.1,.1,.2,.4,.7,1,1.5,2,2.5,3 $season
+				python nao_ngl.py 11 1 $res MSL MSL $input_path 1 $output_path false -3,-2.5,-2,-1.5,-1,-.7,-.4,-.2,-.1,.1,.2,.4,.7,1,1.5,2,2.5,3 $season
+				python nao_ngl.py 16 1 $res MSL MSL $input_path 1 $output_path false -3,-2.5,-2,-1.5,-1,-.7,-.4,-.2,-.1,.1,.2,.4,.7,1,1.5,2,2.5,3 $season
 			done
 		done
 	fi
 
 	if [[ "$todo" == "haus" ]]; then
-		python hausdorf.py 11 16 T159,T511,T1279 U U $in_mistral 1 $out_mistral false -1,0,1 colorbar_TR_15 3
+		python hausdorf.py 11 16 T159,T511,T1279 U U $input_path 1 $output_path false -1,0,1 colorbar_TR_15 3
 	fi
 
         if [[ "$todo" == "forcing" ]]; then
-                python nh_stereo_forcing.py 11 16 dummy sic sic $in_mistral 1 $out_mistral false -.9,-.8,-.7,-.6,-.5,-.4,-.3,-.2,-.1,-0.01,-0 colorbar_red 18 0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1
+                python nh_stereo_forcing.py 11 16 dummy sic sic $input_path 1 $output_path false -.9,-.8,-.7,-.6,-.5,-.4,-.3,-.2,-.1,-0.01,-0 colorbar_red 18 0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1
         fi
 
 
--- PAMIP_res_paper/nh_stereo_diff_bootstrap.py
+++ PAMIP_res_paper/nh_stereo_diff_bootstrap.py
@@ -64,7 +64,7 @@ def bootstrap(xyobs, data1, data2):
 	pvalue = []
 	n = xyobs.shape[0]//2
 	m = xyobs.shape[0]//2
-	B = 100
+	B = 10000
 
 	for bi in tqdm(range(B)):
 		t = dask.delayed(resample)(xyobs,n,m)
@@ -72,19 +72,15 @@ def bootstrap(xyobs, data1, data2):
 	with ProgressBar():
 		tstar = dask.compute(ta)
 	tstar = np.squeeze(np.asarray(tstar), axis = 0)
-	print(tstar.shape)
-	pvalue = np.empty((tstarobs.shape[1],tstarobs.shape[2]))
-	for lat in tqdm(range(0,tstarobs.shape[1])):
-		for lon in range(0,tstarobs.shape[2]):
-			p1 = tstar[:,0,lat,lon][tstar[:,0,lat,lon] >= tstarobs[0,lat,lon]].shape[0]/B
-			p2 = tstar[:,0,lat,lon][tstar[:,0,lat,lon] <= -tstarobs[0,lat,lon]].shape[0]/B
-			p = p1 + p2
-			pvalue[lat,lon] = p
+	print('Bootstrap size',tstar.shape)
+	pvalue = np.empty((tstarobs.shape[0],tstarobs.shape[1]))
+	for lat in tqdm(range(0,tstarobs.shape[0])):
+		for lon in range(0,tstarobs.shape[1]):
+			p1 = tstar[:,lat,lon][tstar[:,lat,lon] >= tstarobs[lat,lon]].shape[0]/B
+			p2 = tstar[:,lat,lon][tstar[:,lat,lon] >= -tstarobs[lat,lon]].shape[0]/B
+			pvalue[lat,lon] = min(p1,p2)
   	return pvalue
 
-
-if str(sys.argv[9]) == "true":
-	print("hashing signifcant changes")
 
 
 if __name__ == '__main__':
@@ -113,159 +109,157 @@ if __name__ == '__main__':
 
 	mapticks=map(float, sys.argv[10].split(','))
 	reslist=map(str, sys.argv[3].split(','))
-	print(reslist)
 	itimes=0
-	fig =  plt.figure(figsize=(9,10.6875))
+	fig =  plt.figure(figsize=(9,6))
+	datadict1 = {}
+	datadict2 = {}
+	datadict3 = {}
+	datadict4 = {}
 
-	for season in [ 'SON', 'DJF', 'MAM', 'JJA' ]:
-		for res in reslist:
-			if res == 'T1279':
-				ensnumber = 60
-			else:
-				ensnumber = 100
-			if paramname == 'synact':
-				datapath1=basepath+res+'/Experiment_'+exp1+'/synact/'
-                                datapath2=basepath+res+'/Experiment_'+exp2+'/synact/'
-				dataset3=[]
-				dataset4=[]
-				data3=[]
-				data4=[]
+	print(reslist)
+	for res in reslist:
+		print('reading files for',res)
+		if res == 'T1279':
+			ensnumber = 300
+		if res == 'T511':
+			ensnumber = 200
+		if res == 'T159':
+			ensnumber = 100
+		if paramname == 'synact':
+			datapath1=basepath+res+'/Experiment_'+exp1+'/synact/'
+			datapath2=basepath+res+'/Experiment_'+exp2+'/synact/'
+			dataset3=[]
+			dataset4=[]
+			data3=[]
+			data4=[]
 
-				# Reading netcdf files
-				ncfile1 = datapath1+'3D_timstd_'+season+'.nc'
-				ncfile2 = datapath2+'3D_timstd_'+season+'.nc'
-				dataset1 = Dataset(ncfile1)
-				dataset2 = Dataset(ncfile2)
-				print ncfile1
-				print ncfile2
+			# Reading netcdf files
+			ncfile1 = datapath1+'3D_timstd_'+season+'.nc'
+			ncfile2 = datapath2+'3D_timstd_'+season+'.nc'
+			dataset1 = Dataset(ncfile1)
+			dataset2 = Dataset(ncfile2)
+			print ncfile1
+			print ncfile2
 
-				if str(sys.argv[9]) == "true":
-					for i in range(ensnumber):
-					     ncfile3 = datapath1+'3D_'+str(i+1).zfill(3)+'_timstd_'+season+'.nc'
-					     ncfile4 = datapath2+'3D_'+str(i+1).zfill(3)+'_timstd_'+season+'.nc'
-					     print ncfile3
-					     dataset3.append(Dataset(ncfile3))
-					     dataset4.append(Dataset(ncfile4))
-			else:
-				datapath1=basepath+res+'/Experiment_'+exp1+'/ensemble_mean/'
-				datapath2=basepath+res+'/Experiment_'+exp2+'/ensemble_mean/'
-				datapath3=basepath+res+'/Experiment_'+exp1+'/'
-				datapath4=basepath+res+'/Experiment_'+exp2+'/'    
-				dataset3=[]
-				dataset4=[]
-				data3=[]
-				data4=[]
-
-				# Reading netcdf files
-				ncfile1 = datapath1+paramname+'_ensmean_'+season+'.nc'
-				ncfile2 = datapath2+paramname+'_ensmean_'+season+'.nc'
-				dataset1 = Dataset(ncfile1) 
-				dataset2 = Dataset(ncfile2) 
-				print ncfile1
-				print ncfile2
-
-				if str(sys.argv[9]) == "true":
-					for i in range(ensnumber):
-						ncfile3 = datapath3+'E'+str(i+1).zfill(3)+'/outdata/oifs/seasonal_mean/'+paramname+'_'+season+'.nc'
-						ncfile4 = datapath4+'E'+str(i+1).zfill(3)+'/outdata/oifs/seasonal_mean/'+paramname+'_'+season+'.nc'
-						dataset3.append(Dataset(ncfile3))
-						dataset4.append(Dataset(ncfile4))
-						
-			# Loading data from datasets
-			data1 = dataset1.variables[param][:]
-			data2 = dataset2.variables[param][:]
 			if str(sys.argv[9]) == "true":
 				for i in range(ensnumber):
-					data3.append(dataset3[i].variables[param][:])
-					data4.append(dataset4[i].variables[param][:])
+				     ncfile3 = datapath1+'3D_'+str(i+1).zfill(3)+'_timstd_'+season+'.nc'
+				     ncfile4 = datapath2+'3D_'+str(i+1).zfill(3)+'_timstd_'+season+'.nc'
+				     print ncfile3
+				     dataset3.append(Dataset(ncfile3))
+				     dataset4.append(Dataset(ncfile4))
+		else:
+			datapath3=basepath+res+'/Experiment_'+exp1+'/'
+			datapath4=basepath+res+'/Experiment_'+exp2+'/'    
+			data3=[]
+			data4=[]
 
+			for i in tqdm(range(ensnumber)):
+				ncfile3 = datapath3+'E'+str(i+1).zfill(3)+'/outdata/oifs/djfm_mean/'+paramname+'_djfm_mean.nc'
+				ncfile4 = datapath4+'E'+str(i+1).zfill(3)+'/outdata/oifs/djfm_mean/'+paramname+'_djfm_mean.nc'
+
+				data3.append(Dataset(ncfile3).variables[param][:])
+				data4.append(Dataset(ncfile4).variables[param][:])
 			# in case data has multiple levels, select only the (50000 hPa) 
-			if (len(np.squeeze(data1).shape)) == 3:
-				if [ res == 'T1279' ]:
-					data1 = data1[0,4,:,:]
-					data2 = data2[0,4,:,:]
-				else:
-					data1 = data1[0,5,:,:]
-					data2 = data2[0,5,:,:]
-				for i in range(ensnumber):
+				if paramname == 'Z' or paramname == 'U' or paramname == 'V' or paramname == 'T':
 					if [ res == 'T1279' ]:
 						data3[i] =  data3[i][0,4,:,:]
 						data4[i] =  data4[i][0,4,:,:]
 					else:
 						data3[i] =  data3[i][0,5,:,:]
 						data4[i] =  data4[i][0,5,:,:]
+				else:
+					data3[i] = data3[i][0,:,:]
+					data4[i] = data4[i][0,:,:]
 
-			# Calculating Bootstrap test
-			xyobs = np.asarray(np.concatenate([data3,data4]))
-			t.tic()
-			print(np.asarray(data1).shape)
-			pvalue = bootstrap(xyobs, data1, data2)
-			t.toc()
-			print(t.elapsed)
+		data1 = np.mean(np.asarray(data3),axis=0)
+		data2 = np.mean(np.asarray(data4),axis=0)
 
-			# Split data and concatenate in reverse order to turn by 180° to Prime meridian
-			ds1,ds2 = np.hsplit(np.squeeze(data1),2)
-			data_cat1 = np.concatenate((ds2,ds1),axis=1)/float(sys.argv[7])
-			ds1,ds2 = np.hsplit(np.squeeze(data2),2)
-			data_cat2 = np.concatenate((ds2,ds1),axis=1)/float(sys.argv[7])
-			if str(sys.argv[9]) == "true":
-				ds1,ds2 = np.hsplit(np.squeeze(pvalue),2)
-				data_cat3 = np.concatenate((ds2,ds1),axis=1)
-
-			# Loading coords, turning longitude coordiante by 180° to Prime meridian
-			lons = dataset1.variables[u'lon'][:]-180
-			lats = dataset1.variables[u'lat'][:]
-
-			# add cyclic point to data array and longitude
-			data_cat1 ,lons = Ngl.add_cyclic(data_cat1, lons)		
-			data_cat2 = Ngl.add_cyclic(data_cat2)	
-			if str(sys.argv[9]) == "true":
-				data_cat3 = Ngl.add_cyclic(data_cat3)	
-
-			# Calculate where the standard deviation of dataset 1 is larger than the difference between 1 and 2
-			if str(sys.argv[9]) == "true":
-				data4 = abs(data_cat3) < 0.05
-				# Where the absolute value of data2-data1 is smaller than the smalles maptick we don't want to plot significance
-				#data4[abs(data_cat2-data_cat1) < mapticks[(int(len(mapticks))/2)]] = False
-				print( mapticks[(int(len(mapticks))//2)])
-
-			# Set position of subplot and some general settings for cartopy
-			ax=plt.subplot(4,len(reslist),itimes+1,projection=ccrs.NorthPolarStereo())
-			ax.set_extent([-180, 180, area, area], ccrs.PlateCarree())
-			ax.add_feature(cfeature.COASTLINE)
-
-			# Configuring cartopy gridlines
-			gl = ax.gridlines(crs=ccrs.PlateCarree(), linewidth=0.3, color='black',  alpha=0.5)
-			gl.xlocator = mticker.FixedLocator([-180,-135,-90,-45,0,45,90,135,180])
-			gl.ylocator = mticker.FixedLocator([80, 60, 30, 0])
-
-			cmap_TR.set_over("darkred")
-			cmap_TR.set_under("deeppink")
-
-			# Plotting
-			#if str(sys.argv[9]) == "true":
-			#	im=plt.contourf(lons, lats, data4, hatches=[' ','....'],cmap=cmap_TR, extend='both',transform=ccrs.PlateCarree(),zorder=2, alpha=0)
-			#im=plt.contourf(lons, lats, data_cat2-data_cat1*debug, levels=mapticks, cmap=cmap_TR, extend='both',transform=ccrs.PlateCarree(),zorder=1)
-			im=plt.contourf(lons, lats, data_cat3, levels=mapticks, cmap=cmap_TR, extend='both',transform=ccrs.PlateCarree(),zorder=1)
+		datadict1[res] = data1 
+		datadict2[res] = data2 
+		datadict3[res] = data3 
+		datadict4[res] = data4 
 			
-			# Adding text labels
-                        if ( itimes == 0 and res == 'T159' ):
-                                plt.text(0.50, 1.05, 'TL159', horizontalalignment='center', fontsize=18, transform=ax.transAxes)
-                        if ( itimes == 1 and res == 'T511' ):
-                                plt.text(0.50, 1.05, 'TL511', horizontalalignment='center', fontsize=18, transform=ax.transAxes)
-                        if ( itimes == 2 and res == 'T1279' ):
-                                plt.text(0.50, 1.05, 'TL1279', horizontalalignment='center', fontsize=18, transform=ax.transAxes)
-                        if ( itimes % 3 == 0 ):
-                                plt.text(-0.05, .50, season, horizontalalignment='right', fontsize=18, transform=ax.transAxes)
+	for plot in [ 'T159', 'T511', 'T1279', 'T511-T159', 'T1279-T159', 'T1279-T511' ]:
 
-                        # Increment plot counter
-                        itimes=itimes+1
+
+		if plot == 'T159' or plot == 'T511' or plot == 'T1279':
+			data1 = datadict1[plot]
+			data2 = datadict2[plot]
+			data3 = datadict3[plot]
+			data4 = datadict4[plot]
+		if plot == 'T511-T159':
+			data1 = datadict2['T159']-datadict1['T159']
+			data2 = datadict2['T511']-datadict1['T511']
+			data3 = np.asarray(datadict4['T159'])-np.asarray(datadict3['T159'])
+			data4 = np.asarray(datadict4['T511'])-np.asarray(datadict3['T511'])
+		if plot == 'T1279-T159':
+			data1 = datadict2['T159']-datadict1['T159']
+			data2 = datadict2['T1279']-datadict1['T1279']
+			data3 = np.asarray(datadict4['T159'])-np.asarray(datadict3['T159'])
+			data4 = np.asarray(datadict4['T1279'])-np.asarray(datadict3['T1279'])
+		if plot == 'T1279-T511':
+			data1 = datadict2['T511']-datadict1['T511']
+			data2 = datadict2['T1279']-datadict1['T1279']
+			data3 = np.asarray(datadict4['T511'])-np.asarray(datadict3['T511'])
+			data4 = np.asarray(datadict4['T1279'])-np.asarray(datadict3['T1279'])
+
+
+		# Calculating Bootstrap test
+		xyobs = np.asarray(np.concatenate([data3,data4]))
+		t.tic()
+		pvalue = bootstrap(xyobs, data1, data2)
+		t.toc()
+		print(t.elapsed)
+
+		# Split data and concatenate in reverse order to turn by 180° to Prime meridian
+		ds1,ds2 = np.hsplit(np.squeeze(data1),2)
+		data_cat1 = np.concatenate((ds2,ds1),axis=1)/float(sys.argv[7])
+		ds1,ds2 = np.hsplit(np.squeeze(data2),2)
+		data_cat2 = np.concatenate((ds2,ds1),axis=1)/float(sys.argv[7])
+		ds1,ds2 = np.hsplit(np.squeeze(pvalue),2)
+		data_cat3 = np.concatenate((ds2,ds1),axis=1)
+
+		# Loading coords, turning longitude coordiante by 180° to Prime meridian
+		lons = Dataset(ncfile3).variables[u'lon'][:]-180
+		lats = Dataset(ncfile3).variables[u'lat'][:]
+
+		# add cyclic point to data array, pvalue and longitude
+		data_cat1 ,lons = Ngl.add_cyclic(data_cat1, lons)		
+		data_cat2 = Ngl.add_cyclic(data_cat2)	
+		data_cat3 = Ngl.add_cyclic(data_cat3)	
+
+		# Set position of subplot and some general settings for cartopy
+		ax=plt.subplot(2,3,itimes+1,projection=ccrs.NorthPolarStereo())
+		ax.set_extent([-180, 180, area, area], ccrs.PlateCarree())
+		ax.add_feature(cfeature.COASTLINE)
+
+		# Configuring cartopy gridlines
+		gl = ax.gridlines(crs=ccrs.PlateCarree(), linewidth=0.3, color='black',  alpha=0.5)
+		gl.xlocator = mticker.FixedLocator([-180,-135,-90,-45,0,45,90,135,180])
+		gl.ylocator = mticker.FixedLocator([80, 60, 30, 0])
+
+		# Overflow colors
+		cmap_TR.set_over("darkred")
+		cmap_TR.set_under("deeppink")
+
+		# Plotting
+		s5=plt.contour(lons, lats, np.squeeze(data_cat3), levels=[0,0.025], linestyles='-' ,colors='black',transform=ccrs.PlateCarree() ,zorder=3)
+		s20=plt.contour(lons, lats, np.squeeze(data_cat3), levels=[0,0.1], linestyles='--' ,colors='black',transform=ccrs.PlateCarree() ,zorder=2)
+		im=plt.contourf(lons, lats, data_cat2-data_cat1*debug, levels=mapticks, cmap=cmap_TR, extend='both',transform=ccrs.PlateCarree(),zorder=1)
+		
+		# Adding text labels
+		plt.text(0.50, 1.05, plot, horizontalalignment='center', fontsize=18, transform=ax.transAxes)
+
+		# Increment plot counter
+		itimes=itimes+1
 
 fig.subplots_adjust(hspace=-0.1, wspace = 0.1, left = 0.1, right = 0.85, top = 0.95, bottom = 0.05)
-cbar_ax = fig.add_axes([0.88, 0.16, 0.03, 0.67])
+cbar_ax = fig.add_axes([0.88, 0.16, 0.023, 0.67])
 cbar_ax.tick_params(labelsize=int(sys.argv[12])) 
-fig.colorbar(im, cax=cbar_ax, orientation='vertical', extend='both',ticks=mapticks)
-fig.savefig(outpath+paramname+'_'+exp2+'_'+exp1+'_map_diff.png', dpi=150)
+colorbar_format = '% 1.1f'
+fig.colorbar(im, cax=cbar_ax, orientation='vertical', extend='both',ticks=mapticks,format=colorbar_format)
+fig.savefig(outpath+paramname+'_'+exp2+'_'+exp1+'_map_diff.png', dpi=300)
 
 
 
